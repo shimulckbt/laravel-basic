08463153e7ba4d3fa1b94f467396eaa447d04099
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-04-07 17:28:57 +0600

    resource route

--- api-prac-9/routes/api.php
+++ api-prac-9/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+// Route::get('message', function () {
+//     return response()->json([
+//         'message' => 'welcome to laravel api'
+//     ], 422);
+// });
+
+
+// Route::get('categories', [CategoryController::class, 'index']);
+// Route::post('categories/store', [CategoryController::class, 'store']);
+
+Route::resource('categories', CategoryController::class);
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
