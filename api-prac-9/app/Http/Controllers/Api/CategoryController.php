c8cf89d13033bf5f0908553cc55a60fa3d187fe0
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-04-07 18:29:13 +0600

    create method

--- api-prac-9/app/Http/Controllers/Api/CategoryController.php
+++ api-prac-9/app/Http/Controllers/Api/CategoryController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class CategoryController extends Controller
 {
@@ -19,19 +21,50 @@ class CategoryController extends Controller
         ]);
     }
 
-    public function create()
-    {
-        //
-    }
-
     public function store(Request $request)
     {
-        //
+        // $request->validate([
+        //     'name' => 'required|string|unique:categories',
+        // ]);
+
+        $data = Validator::make($request->all(), [
+            'name' => 'required|string|unique:categories',
+        ]);
+
+        if ($data->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error',
+                'errors' => $data->getMessageBag(),
+            ], 405);
+        }
+        $formData = $data->validated();
+        $formData['slug'] = Str::slug($formData['name']);
+        $cat = Category::create($formData);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Successfully created',
+            'data' => $cat,
+        ], 405);
     }
 
     public function show($id)
     {
-        //
+        $cat = Category::find($id);
+        if (!$cat) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Not Found ',
+                'errors' => [],
+            ], 404);
+        } else {
+            return response()->json([
+                'success' => true,
+                'message' => 'Successfully found',
+                'data' => $cat,
+            ], 405);
+        }
     }
 
 
