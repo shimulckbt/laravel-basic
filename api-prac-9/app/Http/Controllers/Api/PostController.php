47592cd630618b8a60f8c3d1fca85d986baebafb
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-04-13 22:53:19 +0600

    post index method

--- api-prac-9/app/Http/Controllers/Api/PostController.php
+++ api-prac-9/app/Http/Controllers/Api/PostController.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::latest('id')->get();
+        return response()->json([
+            'success' => true,
+            'message' => 'All posts',
+            'data' => $posts,
+        ]);
+    }
 }
