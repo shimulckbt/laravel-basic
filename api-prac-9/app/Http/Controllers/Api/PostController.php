bcd6a4b492a85dcdf68b97f468120bf6d3a0856b
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-04-13 22:54:01 +0600

    post store method

--- api-prac-9/app/Http/Controllers/Api/PostController.php
+++ api-prac-9/app/Http/Controllers/Api/PostController.php
@@ -20,4 +20,41 @@ class PostController extends Controller
             'data' => $posts,
         ]);
     }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:180|unique:posts',
+            'photo' => 'nullable|image|mimes:jpg,jpeg,png|max:2048',
+            'content' => 'required|string',
+        ]);
+
+        // return form validation error with json if error occured
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error occured',
+                'errors' => $validator->getMessageBag(),
+            ], 422);
+        }
+
+        $data = $validator->validated();
+
+        // add post slug
+        $data['slug'] = Str::slug($data['title']);
+
+        // photo will upload if exist
+        if (array_key_exists('photo', $data)) {
+            $data['photo'] = Storage::putFile('', $data['photo']);
+        }
+
+        // store a new post
+        Post::create($data);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Post created successfully!',
+            'data' => [],
+        ], 201);
+    }
 }
